26e3a755711ee8b05d6e1e804e68788c8f16e380
Michael Chapman <gsbc20176@gmail.com>
2024-01-22 15:32:36 -0800

    Fixed crashing. TODO: Determine non case sensitivity.

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -9,31 +9,31 @@ string choice;
 
 
 void game() {
+    //The main function of the program. This section will implement as much as I learn as possible. I will also have easter eggs once I learn how to launch a program with C++
+    //I truly think it would be hilarious if you type portal and portal opens. Maybe I'll even write an auto hotkey to beat the game.
     cout << "Hello User. Welcome to The Dungeon\n";
     cout << "NOTE: Due to my ineptitude, this program is CASE SENSITIVE\n";
     cout << "This portion of the program is fully copyrighted. \n";
     cout << "What is your name Player?";
+    //This block is where the actual game starts
     getline(cin, userInput);
     cout << "Thank you " << userInput << " for joining me today\n";
     cout << "You are in a field. \nIt is getting dark. \nYou are likely to be eaten by a grue.\n";
     cout << "You can go North, South, or Look around\n";
+    //This is where it gets intresting. Nested if loops and checks are the only way I know how to do this thing
     getline(cin, choice);
     if (choice == "North")
-    {
+    {  //the north if loop
         cout << "You head north. There is a mailbox in front of you. You can continue North to reach a house.\n";
+        cout << "You then notice a sign that reads 'The mailbox is broken'\n";
         getline(cin, choice);
-        if (choice == "mailbox") 
-        {
-            cout << "Inside the mailbox, you find a flyer.\n";
-            getline(cin, choice);
-            if (choice.find("read") || choice.find("yes"))
-            {
-                cout << "This is the dungeon. Find the orb of Gaben and escape.\n Watch out for the Grue\n";
-                }
+        if (choice == "North") {
+            cout << "You enter the house. There are stairs to your Left. You can see a kitchen ahead, and a dark room to your right.\n";
+            cout << "The place stinks of grue\n";
         }
     }
-    else {
-        if (choice == "South")
+        else {
+         if (choice == "South")
         {
             cout << "You stumble forward blindly, and break your nose on the Grue. He eats you. Game Over";
         }
@@ -46,7 +46,7 @@ void game() {
 
 
 
-int main()
+    int main()
 {
     cout << "Hello World!\n";
     cout << "This Program Copyright 2024 Trolololol Productions\n";
@@ -55,17 +55,19 @@ int main()
     if (userInput == "Nobody")
     {
         game();
+        //This calls the game, which is the truly main program. This portion will be updated as I learn new things.
     }
     else {
         if (userInput == "Brian")
         {
             cout << "Have a great day Professor!";
-            return 0;
+            
         }
         else {
 
-            return 0;
+           //do something here at some point
         }
     }
 }
+
 
