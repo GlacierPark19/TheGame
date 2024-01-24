4c23e0ff7955ad7ba5b21eb96ec0493cb055429e
Michael Chapman <gsbc20176@gmail.com>
2024-01-24 15:45:33 -0800

    Fixed case sensitivity

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -4,38 +4,53 @@
 
 
 #include <iostream>
-#include "libraries"
+#include "libraries.h"
 #include <string>
+#include "functions.h"
 #include <windows.h>
 #include <shellapi.h>
 #include "Music.h"
 using namespace std;
 string userInput;
 string choice;
+string name;
 
+void takeInput() {
+    getline(cin, choice);
+    transform(choice.begin(), choice.end(), choice.begin(), ::towlower);
+    //This function fixes issues regarding capitilization.
+    //Hopefully this works :)
+}
 
 void game() {
     //The main function of the program. This section will implement as much as I learn as possible. I will also have easter eggs once I learn how to launch a program with C++
     //I truly think it would be hilarious if you type portal and portal opens. Maybe I'll even write an auto hotkey to beat the game.
     cout << "Hello User. Welcome to The Dungeon\n";
-    cout << "NOTE: Due to my ineptitude, this program is CASE SENSITIVE\n";
+    //cout << "NOTE: Due to my ineptitude, this program is CASE SENSITIVE\n";
     cout << "This portion of the program is fully copyrighted. \n";
     cout << "What is your name Player? ";
     //This block is where the actual game starts
-    getline(cin, userInput);
-    transform(userInp)
-    cout << "Thank you " << userInput << " for joining me today\n";
+    getline(cin, name);
+    cout << "Thank you " << name << " for joining me today\n";
     cout << "You are in a field. \nIt is getting dark. \nYou are likely to be eaten by a grue.\n";
     cout << "You can go North, South, or Look around\n";
     //This is where it gets intresting. Nested if loops and checks are the only way I know how to do this thing
-    getline(cin, choice);
-    
-    if (choice == "North")
+    takeInput();
+    if (choice == "north")
     {  //the north if loop
         cout << "You head north. There is a mailbox in front of you. You can continue North to reach a house.\n";
-        cout << "You then notice a sign that reads 'The mailbox is broken'\n";
-        getline(cin, choice);
-        if (choice == "North") {
+        cout << "You can also read the mail.\n";
+        takeInput();
+            if (choice == "read") {
+                cout << "Thank you so much for stumbling upon my little game.\n The majority of this is Copyrighted using the GNU public licence.\n THis program uses many public libraries.\n";
+                cout << "Some sections of this game are not yet accessable.\n Sorry bout that...";
+                cout << "There is also a flyer. It reads:\n";
+                cout << "Shop LTTSTORE.com\n";
+                }
+        else {
+
+        }
+        if (choice == "north") {
             cout << "You enter the house. There are stairs to your Left. You can see a kitchen ahead, and a dark room to your right.\n";
             cout << "The place stinks of grue\n";
         }
@@ -54,9 +69,6 @@ void game() {
         }
     }
 }
-
-
-
     int main()
 {
     cout << "Hello World!\n";
---
+++ functions.h
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "libraries.h"
+#include <string>
+#include <windows.h>
+#include <shellapi.h>
+
+using namespace std;
