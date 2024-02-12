c13d27cc8b6082f0b78e359905655c22c0f483dc
Michael <michael@chapmanmedia.org>
2024-02-12 14:50:54 -0800

    Improvements being made:
    Code Cleanup of old systems
    Continuing Switch Case Work
    REMOVED-Old Main Direction Code

--- TheGame.cpp
+++ TheGame.cpp
@@ -1,73 +1,53 @@
 // HelloWorld.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//This program was developed with assistance from W3 schools (w3schools.com/cpp)
+// This program was developed with assistance from W3 schools (w3schools.com/cpp)
 #include "Depends/libraries.h"
 #include "Depends/Music.h" //NYI
 using namespace std;
 #include "Depends/vars.h" // VARs
-//Functions
+// Functions
 #include "Depends/functions.h"
 #include "Depends/definitions.h"
-//This section is for using external files. DO NOT place items AFTER the directions.
-//Deaths
+// This section is for using external files. DO NOT place items AFTER the directions.
+// Deaths
 #include "Depends/death.h"
 #include "grue.h"
-//Direction Declarations
+// Direction Declarations
 #include "north.cpp"
 #include "south.cpp"
 
-
-void game() {
-    //The main function of the program. This section will implement as much as I learn as possible. I will also have easter eggs once I learn how to launch a program with C++
-    //I truly think it would be hilarious if you type portal and portal opens. Maybe I'll even write an auto hotkey to beat the game.
+void game()
+{
+    // The main function of the program. This section will implement as much as I learn as possible. I will also have easter eggs once I learn how to launch a program with C++
+    // I truly think it would be hilarious if you type portal and portal opens. Maybe I'll even write an auto hotkey to beat the game.
     cout << "Hello User. Welcome to The Dungeon\n";
     cout << "a";
     cout << "This portion of the program is fully copyrighted. \n";
     cout << "What is your name Player? ";
-    //This block is where the actual game starts
+    // This block is where the actual game starts
     getline(cin, name);
     cout << "Thank you " << name << " for joining me today\n";
     cout << "You are in a field. \nIt is getting dark. \nYou are likely to be eaten by a grue.\n";
     cout << "You can go North, South, or Look around\n";
-    //This is where it gets intresting. Nested if loops and checks are the only way I know how to do this thing
+    // This is where it gets intresting. Nested if loops and checks are the only way I know how to do this thing
     takeInput();
-    switch (choiceEnum) {
-    case 1: 
-        north(); 
+    switch (choiceEnum)
+    {
+    case 1:
+        north();
         break;
-    case 2: 
+    case 2:
         south();
         break;
     case 5:
         return;
     default:
         return;
-    
     }
-  /*  if (choice == "north")
-    {
-        north();
-    }
-    else {
-        if (choice == "south")
-        {
-            south();
-        }
-        else {
-
-            if (choice == "look")
-            {
-                cout << "Finally, somebody made the correct choice. \nTo your north, is a house. To the South is a Dark Forest. It smells.... Funky)\n";
-                return;
-            }
-        }
-    }*/
-
 }
 
-
-    int main()
+int main()
 {
-        cout << "Launching game...\n";
-        cout << "Please Wait...\n";
-        game();
+    cout << "Launching game...\n";
+    cout << "Please Wait...\n";
+    game();
 }
